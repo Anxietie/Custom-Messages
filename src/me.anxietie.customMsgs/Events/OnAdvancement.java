9263d0dac6e672431716bd74c64d41df9de0a08b
Anxietie <84636109+Anxietie@users.noreply.github.com>
2022-02-03 18:51:43 -0500

    Create OnAdvancement.java

---
+++ src/me.anxietie.customMsgs/Events/OnAdvancement.java
@@ -0,0 +1,62 @@
+package me.anxietie.customMsgs.Events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventException;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+
+import me.anxietie.customMsgs.Main;
+
+import org.bukkit.ChatColor;
+
+public class OnAdvancement implements Listener{
+	
+	private final Main main;
+	
+	public OnAdvancement(Main main) {
+		this.main = main;
+	}
+	
+	// When player completes an advancement, this is called
+	@EventHandler
+	public void OnPlayerAdvancement(PlayerAdvancementDoneEvent event) throws EventException {
+		if (event == null)
+			return;
+		// Name of the advancement (example: "story/root" or "story/mine_diamond")
+		String adv = event.getAdvancement().getKey().getKey();
+		if (adv.contains("recipes"))
+			return;
+		String message = main.getConfig().getString("adv-prefix");
+		// Replaces the '/' with a '-' to match the paths in the config
+		adv = adv.replace('/', '-');
+		// If they want to use the universal message
+		if (main.getConfig().getBoolean("use-universal-adv-msg")) {
+			// In a try-catch because of weird errors thrown
+			try {
+				// Builds the message and broadcasts it
+				message += main.getConfig().getString("universal-adv-msg");
+				if (message.contains("%player%"))
+					message = message.replaceAll("%player%", event.getPlayer().getName());
+				if (message.contains("%advancement%"))
+					message = message.replaceAll("%advancement%", main.getConfig().getString(adv));
+				Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', message));
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				main.getLogger().info("Error getting advancement for "+event.getPlayer().getName());
+			}
+		}
+		// If they don't want to use the universal message
+		else {
+			try {
+				message = main.getConfig().getString(adv);
+				Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', message));
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				main.getLogger().info("Error getting advancement for "+event.getPlayer().getName());
+			}
+		}
+	}
+}
