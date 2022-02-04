69413c07a150b09ea33a28a8d5f3a64ae60b2a2e
Anxietie <84636109+Anxietie@users.noreply.github.com>
2022-02-03 20:57:23 -0500

    Update OnDeath.java

--- src/me.anxietie.customMsgs/Events/OnDeath.java
+++ src/me.anxietie.customMsgs/Events/OnDeath.java
@@ -6,6 +6,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 import me.anxietie.customMsgs.Main;
+import org.bukkit.ChatColor;
 
 public class OnDeath implements Listener{
 	
@@ -17,6 +18,10 @@ public class OnDeath implements Listener{
 
 	@EventHandler
 	public void onPlayerDeath(PlayerDeathEvent event) throws EventException {
+		// If they have them disabled
+		if (!main.getConfig().getBoolean("dmsg-enabled"))
+			return;
+		
 		if (event == null)
 			return;
 		// Gets the cause of death
@@ -27,12 +32,12 @@ public class OnDeath implements Listener{
 			// In a try-catch because of errors thrown
 			try {
 				// Builds the message based off the values in the config
-				message += main.getConfig().getString("universal-death-msg");
+				message = main.getConfig().getString("universal-death-msg");
 				if (message.contains("%player%"))
 					message = message.replaceAll("%player%", event.getEntity().getName());
 				if (message.contains("%cause%"))
 					message = message.replaceAll("%cause%", main.getConfig().getString(cause));
-				event.setDeathMessage(message);
+				event.setDeathMessage(ChatColor.translateAlternateColorCodes('&', message));
 			}
 			catch (Exception e) {
 				e.printStackTrace();
@@ -42,9 +47,10 @@ public class OnDeath implements Listener{
 		// If they don't want to use the universal message
 		else {
 			try {
-				message = main.getConfig().getString(cause);
-				message = message.replaceAll("%player%", event.getEntity().getName());
-				event.setDeathMessage(message);
+				message += main.getConfig().getString(cause);
+				if (message.contains("%player%"))
+					message = message.replaceAll("%player%", event.getEntity().getName());
+				event.setDeathMessage(ChatColor.translateAlternateColorCodes('&', message));
 			}
 			catch (Exception e) {
 				e.printStackTrace();
